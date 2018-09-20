8da770ce5901f7cb6c7bdfb4ddaf9aba06a81087
libowen <libowen@xiaomi.com>
2018-09-20 15:29:05 +0800

    引入bootstrap jquery 简单登陆页面实现

--- src/main/java/com/bowen/blog/controller/TestController.java
+++ src/main/java/com/bowen/blog/controller/TestController.java
@@ -1,20 +1,16 @@
 package com.bowen.blog.controller;
 
-import com.bowen.blog.proxy.BlogProxy;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by libowen on 2018/7/9.
  */
-@RestController
+@Controller
 public class TestController {
-    @Autowired
-    private BlogProxy blogProxy;
 
     @RequestMapping("/")
-    public Object test() {
-        return "hello world";
+    public Object login() {
+        return "login";
     }
 }
